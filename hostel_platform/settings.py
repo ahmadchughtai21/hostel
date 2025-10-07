af4132fb47b7ac79e11267073f3433f9c3b164d1
ahmadchughtai21 <ahmadchughtai21@gmail.com>
2025-10-07 16:56:15 +0500

    updates

--- hostel_platform/settings.py
+++ hostel_platform/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rw&ixpvios3$tncz7-&5cni5r^zg*@a1#b+b7r6ycd7_(!n&-m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["www.hostelza.com","hostelza.com","localhost","127.0.0.1"]
 
 
 # Application definition
